ce065a8d4fdcc0ca267f440133297e86e69d99a0
caiordjesus <caiordjesus@gmail.com>
2021-11-13 14:55:53 -0300

    Application automatic deploy

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
-import colors from './assets/colors';
+// import colors from './assets/colors';
 
 export default async function init(app: any) {
   try {
-    app.listen(process.env.PORT || 3001, () => {
-      console.log(colors.bright, colors.fgWhite, 'Express App Listening on Port 3001');
-    });
+    app.listen(process.env.PORT || 3001);
   } catch (error) {
     console.error(`An error occurred: ${JSON.stringify(error)}`);
     process.exit(1);
