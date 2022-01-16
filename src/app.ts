f0498e6951fb2c09c006318116da47539fec3141
caiordjesus <caiordjesus@gmail.com>
2022-01-16 15:54:11 -0300

    Application automatic deploy

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
 require('module-alias/register')
 import express from 'express';
 import bodyParser from 'body-parser';
-
+import dotenv from 'dotenv'
 import routes from './route_index';
-
 import route_map from './assets/route_map'
-
 import server from './server'
 
 const app = express()
-app.use(routes)
 
+dotenv.config()
+
+app.use(routes)
 app.use(bodyParser.json())
 
 route_map(app)
--- src/global/config/redis_config.ts
+++ src/global/config/redis_config.ts
@@ -1,12 +1,9 @@
 import { createClient } from 'redis';
 
-
-export const redisConfig = {
-    url: 'redis://:kJEdJUy4bkwG1baXFJ4fZsa0crlpm1UR@redis-17776.c16.us-east-1-3.ec2.cloud.redislabs.com:17776'
-}
-
 export async function redisClientConnect() {
-    const client = createClient(redisConfig);
+    const client = createClient({
+        url: process.env.REDIS_URL
+    });
 
     client.on('error', (err) => console.log('Redis Client Error', err));
 
