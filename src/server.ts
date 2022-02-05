df45d9aba7ef1f7852906c431f4b02e2f4b8669b
Walter Filho <walter.filho@athonenergia.com.br>
2022-02-05 14:30:40 -0300

    Loga porta onde servidor está up

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-// import colors from './assets/colors';
-
 export default async function init(app: any) {
   try {
-    app.listen(process.env.PORT || 3001);
+    const port = process.env.PORT || 3001
+    console.log(`listening on port ${port}`)
+    app.listen(port);
   } catch (error) {
     console.error(`An error occurred: ${JSON.stringify(error)}`);
     process.exit(1);
